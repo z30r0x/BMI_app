0c8789f8cd9b13c754169aee432382083fd9b4d4
Toka Sayed <118456216+z30r0x@users.noreply.github.com>
2026-04-14 13:32:32 +0200

    new

--- lib/features/home_page/home.dart
+++ lib/features/home_page/home.dart
@@ -3,9 +3,14 @@ import 'package:bmi_app/core/utils/images/images.dart';
 import 'package:bmi_app/features/home_page/widgets/main_text.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,29 +21,22 @@ class HomePage extends StatelessWidget {
           children: [
             MainText(),
             SizedBox(height: 30),
-            Text('Please choose your gender', style: TextStyle(fontSize: 24)),
-            TextButton(
-              onPressed: () {},
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 30.0,
-                  bottom: 30.0,
-                  right: 10,
-                  left: 10,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: AppColors.maleCardColor,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  height: 190,
-                  width: 390,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [Text('Male'), Image.asset(Images.male)],
-                  ),
-                ),
-              ),
+            Text('Please choose your gender', style: TextStyle(fontSize: 22)),
+            InkWell(
+              onTap: () {
+                setState() {
+                  Image(
+                    image: AssetImage(Images.male),
+                    width: 370,
+                    height: 190,
+                  );
+                  Image(
+                    image: AssetImage(Images.female),
+                    width: 350,
+                    height: 170,
+                  );
+                }
+              },
             ),
           ],
         ),
